07230fdc43e2ec6bf9a4bb50758e579b6a243142
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2006-09-19 20:20:57 +0000

    Preparing the 0.4 release

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# $Id$
 """pyFormex GUI module initialisation.
 
 Currently, this does nothing. The file should be kept though, because it is
--- gui/scriptsMenu.py
+++ gui/scriptsMenu.py
@@ -22,6 +22,10 @@ class ScriptsMenu(QtGui.QMenu):
         An option to reload the directory is always included.
         """
         QtGui.QMenu.__init__(self,'Examples')
+        self.load(dir)
+        
+
+    def load(self,dir):
         self.dir = dir
         files = os.listdir(dir)
         filter1 = lambda s:s[-3:]==".py" and s[0]!='.' and s[0]!='_'
@@ -29,19 +33,53 @@ class ScriptsMenu(QtGui.QMenu):
         files = filter(filter1,files)
         files = filter(filter2,files)
         files.sort()
+        self.files = map(lambda s:s[:-3],files)
         if GD.options.debug:
             print "Found Examples in %s" % dir
-            print files
-        for f in files:
-            self.addAction(os.path.splitext(f)[0])
+            print self.files
+        for f in self.files:
+            self.addAction(f)
         self.connect(self,QtCore.SIGNAL("triggered(QAction*)"),self.run)
-    
+        self.addSeparator()
+        self.addAction('Run next example',self.runNext)
+        self.addAction('Run all examples',self.runAll)
+        self.addAction('Reload examples',self.reLoad)
+        self.current = ""
+        
 
     def run(self,action):
         """Run the selected example."""
-        selected = os.path.join(self.dir,str(action.text()))+'.py'
-        if GD.options.debug:
-            print "Playing ",selected
+        script = str(action.text())
+        if script in self.files:
+            self.runScript(script)
+    
+
+    def runScript(self,filename):
+        """Run the specified example."""
+        self.current = filename
+        selected = os.path.join(self.dir,filename+'.py')
+        GD.debug("Playing script %s" % selected)
         GD.gui.setcurfile(selected)
         fileMenu.play()
         
+
+    def runAll(self):
+        """Run all examples."""
+        print "Running all examples"
+        for f in self.files:
+            self.runScript(f)
+
+
+    def runNext(self):
+        try:
+            i = self.files.index(self.current) + 1
+        except ValueError:
+            i = 0
+        print "This is example %s out of %s" % (i,len(self.files))
+        if i < len(self.files):
+            self.runScript(self.files[i])
+
+
+    def reLoad(self):
+        pass
+    
