a23b297875a55df64ebdb184168d5be67246624a
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2007-04-25 16:19:27 +0000

    RELEASE 0.4.2

--- globaldata.py
+++ globaldata.py
@@ -10,7 +10,7 @@
 """Global data for pyFormex."""
 
 # Set pyformex version
-__version__ = "0.4.2-rc2"
+__version__ = "0.4.2"
 Version = 'pyFormex %s' % __version__
 options = None
 print_help = None
