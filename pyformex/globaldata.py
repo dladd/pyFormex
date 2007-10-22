89d50d6610bb52b54788a46ec6d827126d89e9c1
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2007-10-22 09:49:02 +0000

    Added a script to install gts

--- pyformex/globaldata.py
+++ pyformex/globaldata.py
@@ -12,7 +12,7 @@
 """Global data for pyFormex."""
 
 # Set pyformex version
-__version__ = "0.6a4"
+__version__ = "0.6a5"
 Version = 'pyFormex %s' % __version__
 Copyright = 'Copyright (C) 2007 Benedict Verhegghe'
 
