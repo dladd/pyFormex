1b387146ffa2a4a65fd0b99413c3cd7df8689863
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2007-11-05 22:08:36 +0000

    More Lima Goodies

--- pyformex/examples/Lima.py
+++ pyformex/examples/Lima.py
@@ -31,7 +31,7 @@ def turtlecmds(rules={}):
 # here are some nice lima generations.
 # Each tuple holds an axiom, grow rules, generations and turtle rules
 limas = {
-    'rule0': [ "F", {"F":"F+G","G":"F-G"},10,turtlecmds() ],
+    'Dragon Curve': [ "F", {"F":"F+G","G":"F-G"},10,turtlecmds() ],
     'Koch Line': [ "F", {"F":"F*F//F*F"},6,turtlecmds() ],
     'rule2': [ "F+F+F+F", {"F":"FF+FF--FF+F"},4,turtlecmds() ],
     'rule3': [ "F+F+F+F", {"F":"FF+F+F-F+FF"},4,turtlecmds() ],
@@ -39,14 +39,16 @@ limas = {
     'rule4': [ "F+F+F+F", {"F":"FF+F++F+F"},4,turtlecmds() ],
     'rule5': [ "F+F+F+F", {"F":"FF+F+F+F+F+F-F"},4,turtlecmds() ],
     'Hilbert Curve': [ "X", {"X":"-YF+XFX+FY-", "Y":"+XF-YFY-FX+"},5,turtlecmds() ],
-    'rule7': [ "F+XF+F+XF", {"X":"XF-F+F-XF+F+XF-F+F-X"},4,turtlecmds() ],
-    'rule8': [ "X", {"X":"XFYFX+F+YFXFY-F-XFYFX", "Y":"YFXFY-F-XFYFX+F+YFXFY"},4,turtlecmds() ],
-    'Gosper Line':      [ "XF", {"X":"X*YF**YF/FX//FXFX/YF*", "Y":"/FX*YFYF**YF*FX//FX/Y"},4,turtlecmds() ],
+    'Greek Cross Curve': [ "F+XF+F+XF", {"X":"XF-F+F-XF+F+XF-F+F-X"},4,turtlecmds() ],
+    'Peano Curve': [ "X", {"X":"XFYFX+F+YFXFY-F-XFYFX", "Y":"YFXFY-F-XFYFX+F+YFXFY"},4,turtlecmds() ],
+    'Gosper Curve':      [ "XF", {"X":"X*YF**YF/FX//FXFX/YF*", "Y":"/FX*YFYF**YF*FX//FX/Y"},4,turtlecmds() ],
     'Sierpinski Triangle': [ "F**F**F", {"F":"F*J++F**F", "J":"JJ"},6,turtlecmds() ],
-    'rule11': [ "F*F*F*F*F*F", {"F":"+F/F*F-"},5,turtlecmds({'+':'ro(20);','-':'ro(-20);'}) ],
-    'rule12': [ "F*F*F*F*F*F/F/F/F/F/F*F*F*F*F*F", {"F":"+F/F*F-"},4,turtlecmds({'+':'ro(20);','-':'ro(-20);'}) ],
-    'plant0': [ "+F", {"F":"F[*F]F[/F]F"},5,turtlecmds({'*':'ro(25);','/':'ro(-25);'}) ],
-    'plant1': [ "+Y", {"Y":"YFX[*Y][/Y]", "X":"[/F][*F]FX"},7,turtlecmds({'*':'ro(25);','/':'ro(-25);'}) ],
+    'Sierpinski Triangle1': [ "F", {"F":"*G/F/G*", "G":"/F*G*F/"},8,turtlecmds() ],
+    'Sierpinski Carpet': [ "F+F+F+F", {"F":"JF+F+F+F+JF+F+F+F+J", "J":"JJJ"},3,turtlecmds() ],
+    'Gosper Island': [ "F*F*F*F*F*F", {"F":"+F/F*F-"},5,turtlecmds({'+':'ro(20);','-':'ro(-20);'}) ],
+    'Gosper Island Tiling': [ "F*F*F*F*F*F/F/F/F/F/F*F*F*F*F*F", {"F":"+F/F*F-"},4,turtlecmds({'+':'ro(20);','-':'ro(-20);'}) ],
+    'Plant0': [ "+F", {"F":"F[*F]F[/F]F"},5,turtlecmds({'*':'ro(25);','/':'ro(-25);'}) ],
+    'Plant1': [ "+Y", {"Y":"YFX[*Y][/Y]", "X":"[/F][*F]FX"},7,turtlecmds({'*':'ro(25);','/':'ro(-25);'}) ],
     'Breezy Bush': [ "+F", {"F":"FF[//F*F*F][*F/F/F]"},4,turtlecmds({'*':'ro(22.55);','/':'ro(-22.5);'}) ],
     'Islands and Lakes': [ "F-F-F-F", {"F":"F-J+FF-F-FF-FJ-FF+J-FF+F+FF+FJ+FFF", "J":"JJJJJJ"},2,turtlecmds() ],
     'Hexagones': [ "F*F*F*F*F*F", {"F":"[//J*G*F*G]J", "G":"[//K*G*F*G]J"},5,turtlecmds() ],
@@ -54,27 +56,33 @@ limas = {
     'rule19': [ "F++F", {"F":"*F//F*"}, 10, turtlecmds({'*':'ro(30);','/':'ro(-30);'}) ],
     'rule20': [ "F+F+F+F", {"F":"*F//G*","G":"/F**G/"}, 8, turtlecmds({'*':'ro(30);','/':'ro(-30);'}) ],
     'rule21': [ "G+G+G+G", {"F":"*F//G*","G":"/F**G/"}, 8, turtlecmds({'*':'ro(30);','/':'ro(-30);'}) ],
+    'Grass': [ "***X", { "F":"FF", "X":"F*[[X]/X]/F[/FX]*X" }, 6, turtlecmds({'*':'ro(25);','/':'ro(-25);'}) ],
     #22: [ "+F", {"F":"GH", "G":"GG", "H":"G[*F][/F]"}, 12, turtlecmds({'*':'ro(22.5);','/':'ro(-22.5);'}) ],
     #23: [ "F", {"F":"*F-F*"}, 12, turtlecmds({'*':'ro(45);'}) ],
     #24: [ "JF", {"F":"*F-FF+F*","J":"/J"}, 8, turtlecmds({'*':'ro(45);','/':'ro(-45);'}) ],
     #25: [ "F", {"F":"F-F++F-F"}, 4, turtlecmds() ],
     }
 
-def show(i,L,turtle_cmds):
+def show(i,L,turtle_cmds,clear=True):
     """Show the current production of the Lima L."""
     global FA,TA
     turtle_script = L.translate(turtle_cmds)
     coords = turtle.play("reset();" + turtle_script)
     if len(coords) > 0:
-        FB = draw(Formex(coords))
-        undraw(FA)
+        if clear:
+            prop = 0
+        else:
+            prop = i
+        FB = draw(Formex(coords,prop))
+        if clear:
+            undraw(FA)
         FA = FB
         TB = drawtext("Generation %d"%i,20,20)
         undecorate(TA)
         TA = TB
         
 
-def grow(rule):
+def grow(rule,clearing):
     """Show subsequent Lima productions."""
     global FA,TA
     FA = None
@@ -84,22 +92,24 @@ def grow(rule):
     drawtext(rule,20,40)
     a,r,g,t = limas[rule]
     L = lima.Lima(a,r)
-    show(0,L,t)
+    show(0,L,t,clearing)
     for i in range(g):
         L.grow()
-        show(i+1,L,t)
+        show(i+1,L,t,clearing)
 
+keys = limas.keys()
+keys.sort()
+choices = ['__all__','__custom__'] + keys
 
-choices = ['__all__','__custom__'] + limas.keys()
-
-res = askItems([('Production rule',None,'select',choices)])
+res = askItems([('Production rule',None,'select',choices),('clear',True)])
 
 if res:
     rule = res['Production rule']
+    clearing = res['clear']
     if rule == '__all__':
-        for rule in limas.keys():
-            grow(rule)
+        for rule in keys:
+            grow(rule,clearing)
     elif rule == '__all__':
         pass
     else:
-        grow(rule)
+        grow(rule,clearing)
