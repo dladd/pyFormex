17c5fe828dcf539126b430caee644298c8affcf9
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2010-07-23 18:38:02 +0000

    Removing more old manual files

--- pyformex/manual/scripts/WireStent8.py
+++
@@ -1,3 +0,0 @@
-    def all(self):
-        """Return the Formex with all bar elements."""
-        return self.F
--- pyformex/manual/scripts/WireStentParametricExample.py
+++
@@ -1,9 +0,0 @@
-from examples.WireStent import DoubleHelixStent
-
-for i in [16.,32]:
-    for j in [6,10]:
-        for k in [25,50]:
-            stent = DoubleHelixStent(i,40.,0.22,j,k).all()
-            draw(stent,view='iso')
-            pause()
-            clear()
