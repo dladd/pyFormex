7acd91dad2d76705af91eff7c822222107194dce
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2012-08-02 19:25:10 +0000

    updated documentation

--- pyformex/doc/html/_static/scripts/python_intro.py
+++ pyformex/doc/html/_static/scripts/python_intro.py
@@ -14,13 +14,16 @@ for light in [ 'green','yellow','red','black',None]:
     else:
         print 'THE LIGHT IS BROKEN!'
 
-
-
-appreciation = { 0: 'not driving', 30:'slow', 60:'normal', 90:'dangerous', 120:'suicidal'}
+appreciation = {
+    0: 'not driving',
+    30:'slow',
+    60:'normal',
+    90:'dangerous',
+    120:'suicidal'
+    }
 
 for i in range(5):
     speed = 30*i
     print "%s. Driving at speed %s is %s" % (i,speed,appreciation[speed])
-
 
 # End
