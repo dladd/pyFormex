1248f3ff34ba58638d3292b28e0bafbbcc3b03c7
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2013-02-14 12:44:05 +0100

    Fix bug #38327: closeProject
    The fileMenu.closeProject function has been changed: the 'delet' argument
    has been renamed to 'clear', and the clear argument will now also work
    in case 'save=False' is specified. Thus, closeProject(save=False,clear=True)
    will now close the current project without saving the globals and also clear
    all the globals. Only use if you know what your are doing, because that will
    delete all your globals without being saved.

--- pyformex/gui/guimain.py
+++ pyformex/gui/guimain.py
@@ -990,7 +990,7 @@ def exitDialog():
         fileMenu.saveAsProject()
 
     if not res['reopen']:
-        fileMenu.closeProject(save=False,delet=False)
+        fileMenu.closeProject(save=False,clear=False)
 
     return True
 
